976a66ca1cac57cc2f439a45eec56c061d2048f3
Emidlu <52872122+Emidlu@users.noreply.github.com>
2023-06-26 12:58:52 -0300

    Agregado host de render a allowed hosts

--- cartelera/settings.py
+++ cartelera/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-mu7n#z-u6rw%k9ohq+5g-a^3#_d0lnwp!ce0$-n8n%$@=d5!7+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cinema-python-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cartelera-cine-python.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
